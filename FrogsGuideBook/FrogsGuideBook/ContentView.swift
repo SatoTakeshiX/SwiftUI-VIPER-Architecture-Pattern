11b00ed76a1464ee9cf3dbd8882f9243153bed8a
satoutakeshi <sato.takeshi1987@gmail.com>
2020-05-23 15:20:23 +0900

    replace project

---
+++ FrogsGuideBook/FrogsGuideBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FrogsGuideBook
+//
+//  Created by satoutakeshi on 2020/05/23.
+//  Copyright © 2020 satoutakeshi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
